8ea8cd24ba28e8fee8035922fa49fbee695a3d95
roopantkalra <roopantkalra@gmail.com>
2022-07-23 05:17:34 -0600

    created a nested comment on reply button

--- Assets/js/forum.js
+++ Assets/js/forum.js
@@ -3,6 +3,7 @@ const postSubmitButton=document.querySelector('.post-submit-button')
 const questions=document.querySelector('.posted-questions-title')
 
 
+/*Function-postedQuestionTemplate*/
 const postedQuestionTemplate=(questionInputValue)=>{
           if(!questionInputValue){return}
 
@@ -30,9 +31,6 @@ editBtn.addEventListener('click',()=>{
     postedQuestionText.setAttribute('contenteditable',true)
 })
 
-
-
-
 deleteBtn.innerHTML='Delete'
 deleteBtn.style.cssText=
 `padding : 0.5rem;
@@ -47,8 +45,6 @@ cursor:pointer;`
 deleteBtn.addEventListener('click',()=>{
     questionList.remove()
 })
-
-
 
 const postedQuestionsButtons =document.createElement('buttons')
 postedQuestionsButtons.appendChild(editBtn)
@@ -71,7 +67,69 @@ replybutton.style.cssText=
     padding:0.4em;
     font-weight:700;
     background-color: rgba(158, 95, 95, 0.522);
-    border-radius: 10px;`
+    border-radius: 10px;
+    margin-right:1em;`
+
+// Function-replyQuestion
+const replyToQuestion=()=>{
+ const replyText=document.createElement('input')
+ const postReplybutton=document.createElement('button')
+ postReplybutton.innerHTML='POST'
+ postReplybutton.style.cssText=
+ `font-family: 'Roboto',system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+ padding:0.4em;
+ font-weight:700;
+ background-color: rgba(158, 95, 95, 0.522);
+ border-radius: 10px;
+ margin: 0em 0.5em`
+
+   const cancelReplybutton=document.createElement('button')
+   cancelReplybutton.innerHTML='Cancel'
+   cancelReplybutton.style.cssText=
+   `font-family: 'Roboto',system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+   padding:0.4em;
+   font-weight:700;
+   background-color: grey;
+   border-radius: 10px;
+   margin: 0em 0.5em`
+  questionList.append(replyText)
+  questionList.append(postReplybutton)
+  questionList.append(cancelReplybutton)
+
+  postReplybutton.addEventListener('click',()=>{
+    if (!replyText.value) {return}
+    replyText.remove();postReplybutton.remove();cancelReplybutton.remove();replybutton.remove();
+    const PostedReply=document.createElement('span')
+    PostedReply.innerHTML=`***${replyText.value}`
+    console.log(PostedReply)
+    questionList.append(PostedReply)
+    const DeleteReply=document.createElement('button')    
+    DeleteReply.innerHTML='Delete'
+    DeleteReply.style.cssText=
+    `cursor:pointer;
+    background-color: red;
+    color:white; 
+    border-color: transparent;
+    border-radius: 10px;
+     margin: 0.5em;`
+    questionList.appendChild(DeleteReply)
+
+    DeleteReply.addEventListener('click',()=>{
+        PostedReply.remove();postReplybutton.remove();DeleteReply.remove();replybutton.remove();
+        questionList.appendChild(replybutton)
+    })
+})    
+
+cancelReplybutton.addEventListener('click',()=>{
+    replyText.remove();postReplybutton.remove();cancelReplybutton.remove();
+})
+
+}
+
+
+replybutton.addEventListener('click',()=>{
+    replyToQuestion()
+    })
 
 postedQuestions.appendChild(postedQuestionText)
 postedQuestions.appendChild(postedQuestionsButtons)
